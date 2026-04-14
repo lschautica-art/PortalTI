bf8b1b70e335d65a150903c461c3b064efe4fde9
Lauro <Financeiro@NB-LAURO.dcdist.local>
2026-04-14 11:06:12 -0300

    ajuste de functions

--- netlify/functions/news-rh.js
+++ netlify/functions/news-rh.js
@@ -4,7 +4,7 @@ exports.handler = async function handler() {
   return fetchNews({
     endpoint: "/search",
     params: {
-      q: '"mercado de trabalho" OR salarios OR salário OR remuneracao OR remuneração OR beneficios OR benefícios OR "politicas trabalhistas" OR "relações trabalhistas" OR "direitos trabalhistas" OR emprego OR empregos OR "jornada de trabalho" OR home office OR "trabalho híbrido"',
+      q: '"mercado de trabalho" OR salarios OR salario OR remuneracao OR remuneracao OR beneficios OR beneficios OR "politicas trabalhistas" OR "relacoes trabalhistas" OR "direitos trabalhistas" OR emprego OR empregos OR "jornada de trabalho" OR home office OR "trabalho hibrido"',
       lang: "pt",
       country: "br",
       max: "6",
